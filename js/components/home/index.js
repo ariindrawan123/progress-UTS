4b1295175723702d4db70b646f2e24ecf46d69cc
ariindrawan123 <ariindrawan7@gmail.com>
2017-06-08 00:22:43 +0800

    progress UTS

---
+++ js/components/home/index.js
@@ -0,0 +1,115 @@
+
+import React, { Component } from 'react';
+import { TouchableOpacity, StyleSheet, View, ListView, Image, Text } from 'react-native';
+import { connect } from 'react-redux';
+import { Actions, ActionConst } from 'react-native-router-flux';
+import { Container, Header, Title, Content, Button, Icon, Left, Body, Right } from 'native-base';
+import { Grid, Row } from 'react-native-easy-grid';
+
+import { setIndex } from '../../actions/list';
+import { openDrawer } from '../../actions/drawer';
+import styles from './styles';
+
+const moneyIcon = require('../../../images/money.png');
+var URL="http://mhs.rey1024.com/1415051073/list.php";
+
+class Home extends Component {
+
+  constructor(props){
+    super(props);
+    var ds = new 
+    ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state={
+       dataSource: ds,
+    };
+  }
+
+  AmbilDataKoin() {
+    fetch(URL)
+    .then((response) => response.json())
+    .then((responseData) => {
+      this.setState({
+        dataSource: this.state.dataSource.cloneWithRows
+        (responseData),
+      });
+    }) .done();
+  }
+
+  static propTypes = {
+    name: React.PropTypes.string,
+    setIndex: React.PropTypes.func,
+    list: React.PropTypes.arrayOf(React.PropTypes.string),
+    openDrawer: React.PropTypes.func,
+  }
+
+  newPage(index) {
+    this.props.setIndex(index);
+    Actions.blankPage();
+  }
+
+  renderRow(record) {
+    return (
+      <View style={styles.row}>
+        <View style={styles.iconContainer}>
+          <Image source={moneyIcon} style={styles.icon} />
+        </View>
+        <View style={styles.info}>
+          <Text style={styles.items}>{record.nama}</Text>
+          <Text style={styles.address}>{record.alamat}</Text>
+        </View>
+        <View style={styles.total}>
+          <Text style={styles.date}>Total Coin.</Text>
+          <Text style={styles.price}>Rp.{record.jml_coin}</Text>
+        </View>
+      </View>
+    );
+  }
+
+  render() {
+    this.AmbilDataKoin();
+    return (
+      <Container style={styles.container}>
+        <Header>
+          <Left>
+            <Button transparent onPress={() => Actions.login({ type: ActionConst.RESET })}>
+              <Icon active name="power" />
+            </Button>
+          </Left>
+
+          <Body>
+            <Title>{(this.props.name) ? this.props.name : 'Home'}</Title>
+          </Body>
+
+          <Right>
+            <Button transparent onPress={this.props.openDrawer}>
+              <Icon active name="menu" />
+            </Button>
+          </Right>
+        </Header>
+
+        <Content>
+          <Grid style={styles.mt}>
+            <ListView
+          dataSource={this.state.dataSource}
+          renderRow={this.renderRow}
+        />
+          </Grid>
+        </Content>
+      </Container>
+    );
+  }
+}
+
+function bindAction(dispatch) {
+  return {
+    setIndex: index => dispatch(setIndex(index)),
+    openDrawer: () => dispatch(openDrawer()),
+  };
+}
+
+const mapStateToProps = state => ({
+  name: state.user.name,
+  list: state.list.list,
+});
+
+export default connect(mapStateToProps, bindAction)(Home);
---
+++ js/components/login/index.js
@@ -0,0 +1,89 @@
+
+import React, { Component } from 'react';
+import { Image, Alert } from 'react-native';
+import { connect } from 'react-redux';
+import { Container, Content, Item, Input, Button, Icon, View, Text } from 'native-base';
+import { Actions } from 'react-native-router-flux';
+
+import { setUser } from '../../actions/user';
+import styles from './styles';
+
+var SERVER_LOGIN_URL = 'http://mhs.rey1024.com/1415051073/login.php';
+const background = require('../../../images/Coins.png');
+
+class Login extends Component {
+
+  static propTypes = {
+    
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: "",
+    };
+  }
+
+  login() {
+    fetch(SERVER_LOGIN_URL + '?username=' + this.state.username + '&password=' + this.state.password)
+      .then((response) => response.json())
+      .then((responseData) => {
+        var id = responseData.id;
+        if (id === -1) {
+          Alert.alert("Salah Username atau Password!")
+         }
+         else 
+       {
+        Actions.home();
+        }  
+        
+      })
+      .done();
+  }
+
+
+ 
+
+
+  render() {
+    return (
+      <Container>
+        <View style={styles.container}>
+          <Content>
+            <Image source={background} style={styles.shadow}/>
+              <View style={styles.bg}>
+                <Item style={styles.input}>
+                  <Icon active name="person" />
+                  <Input placeholder="username"
+                   text = {this.state.username}
+                    onChangeText={(e) => this.setState({ username: e })} />
+                </Item>
+                <Item style={styles.input}>
+                  <Icon name="unlock" />
+                  <Input
+                    placeholder="PASSWORD"
+                    text = {this.state.password}
+                    onChangeText={(e) => this.setState({ password: e })} 
+                    secureTextEntry
+                  />
+                </Item>
+                <Button style={styles.btn} onPress={() => this.login()}>
+                  <Text>Login</Text>
+                </Button>
+              </View>
+          </Content>
+        </View>
+      </Container>
+    );
+  }
+}
+
+function bindActions(dispatch) {
+  return {
+    
+  };
+}
+
+
+export default connect(null, bindActions)(Login);
